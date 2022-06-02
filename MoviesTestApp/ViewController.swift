898d8b975fcadd7d6cb501d2dbf84233333afdd0
Monim <mbenaicha@m2mgroup.com>
2022-06-02 11:54:53 +0100

    Initial Commit

---
+++ MoviesTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesTestApp
+//
+//  Created by Macbook PRO on 02/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
